5fad751bec7b16989353621e6b98cbf1c9bb269d
Billy Tulpan <wijt14@gmail.com>
2023-12-02 12:21:45 -0500

    Use cors and add second endpoint

--- api/index.js
+++ api/index.js
@@ -1,20 +1,29 @@
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
+const cors = require('cors')
 
 const app = express();
 const port = 3000;
 
-// Define a simple endpoint
-app.get('/api/arjun', (req, res) => {
-    res.json({ message: 'my whip is way faster than your whip' });
-});
-
 // Configure HTTPS options
+
 const options = {
     key: fs.readFileSync('server.key'),
     cert: fs.readFileSync('server.cert'),
 };
+app.use(cors())
+
+// Define a simple endpoint
+app.post('/api/arjun', (req, res) => {
+    res.json({ message: 'my whip is way faster than your whip' });
+    console.log(req.ip)
+});
+
+app.post('/api/fuckeduparjun', (req, res) => {
+    res.json({ message: 'youre doing rediculous stuff in the street'});
+    console.log(req.ip)
+});
 
 // Create an HTTPS server
 const server = https.createServer(options, app);
