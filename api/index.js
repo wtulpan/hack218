808930e716c28b8a22a0730ca7dca2dd62f57d4f
Billy Tulpan <wijt14@gmail.com>
2023-12-02 10:52:37 -0500

    Change api to use https

--- api/index.js
+++ api/index.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
+
 const app = express();
 const port = 3000;
 
 // Define a simple endpoint
 app.get('/api/arjun', (req, res) => {
-    res.json({ message: 'My whip would smoke that slow ass whip' });
+    res.json({ message: 'my whip is way faster than your whip' });
 });
 
+// Configure HTTPS options
+const options = {
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert'),
+};
+
+// Create an HTTPS server
+const server = https.createServer(options, app);
+
 // Start the server
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+server.listen(port, () => {
+    console.log(`Server is running at https://localhost:${port}`);
 });
