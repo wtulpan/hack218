9e6a4964e45286838e41deb7dd1e6805562ab3ff
Jeremy <jgibsongt21@gmail.com>
2023-12-04 18:59:00 -0500

    Added weather forcast

--- frontend/src/modules/ThermostatWidget.tsx
+++ frontend/src/modules/ThermostatWidget.tsx
@@ -5,26 +5,65 @@ import axios from "axios";
 import { getWeather } from "../api/callThermostatApi";
 import config from "../config.json";
 
+interface WeatherData {
+  current: {
+    time: Date;
+    temperature2m: number;
+  };
+  hourly: {
+    time: Date[];
+    temperature2m: Float32Array;
+  };
+}
+
+const formatTime = (inputTime: string): string =>
+  inputTime.replace(/:\d{2}:\d{2}\s/, " ");
+
 function ThermostatWidget() {
-  const [temperature, setTemperature] = useState<number>(999);
+  const [weatherData, setWeatherData] = useState<WeatherData | undefined>();
   const [coolingState, setCoolingState] = useState<string>("Unknown");
 
   const alertList = ["Tryna smoke?", "No way!", "I'm Arjun!"];
   const apiUrl = `https://${config["api-ip"]}:${config["api-port"]}`;
 
+  const fetchData = async () => {
+    try {
+      const weatherData: WeatherData = await getWeather();
+      setWeatherData(weatherData);
+    } catch (error) {
+      console.error("Error fetching temperature:", error);
+    }
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const temperature = await getWeather();
-        setTemperature(temperature);
-      } catch (error) {
-        console.error("Error fetching temperature:", error);
-      }
-    };
     fetchData();
     const intervalId = setInterval(fetchData, 180000);
     return () => clearInterval(intervalId);
   }, []);
+
+  let hourlyNodes = null;
+  if (weatherData) {
+    const currentTime = new Date();
+    const hourly = weatherData.hourly;
+    const next10HoursData = hourly.time
+      .map((time, index) => ({
+        time,
+        temperature: hourly.temperature2m[index],
+      }))
+      .filter((data) => data.time >= currentTime)
+      .slice(0, 10);
+
+    // Map the filtered data into React nodes
+    hourlyNodes = next10HoursData.map((data, index) => (
+      <div key={index}>
+        <div>
+          {formatTime(data.time.toLocaleTimeString())}
+          {" - "}
+          {Math.round(data.temperature)}°F
+        </div>
+      </div>
+    ));
+  }
 
   return (
     <div className="thermostat-widget widget">
@@ -35,8 +74,10 @@ function ThermostatWidget() {
           <div>69</div>
         </div> */}
         <div className="current-temperature">
-          {temperature < 999 && Math.round(temperature)}
+          {weatherData?.current.temperature2m &&
+            Math.round(weatherData?.current.temperature2m)}
         </div>
+        {hourlyNodes}
         {/* <div className="cooling-state">Cooling State: {coolingState}</div> */}
       </div>
     </div>
